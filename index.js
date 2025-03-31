eb87711c1bb9e0cbea4260bdcfcabf7d430d195e
vishwadeep sankpal <vishwadeep108@gmail.com>
2025-03-31 11:41:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express(); //Creates an Express application. The express() function is a top-level function exported by the express module
+
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.send("hello buddy !!");
+});
+
+app.get("/about", (req, res) => {
+  res.send("this is about us page");
+});
+
+app.get("/youtube", (req, res) => {
+  res.send("<h1>Chai aur Code</h1>");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`exaple app running on port ${port}`);
+});
+//The .listen() method in Express.js starts the server and makes it listen for incoming requests on a specified port.
