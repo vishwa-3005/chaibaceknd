9062ad77913f9c322aeb9cbb9994fd5b0dba32a5
vishwadeep sankpal <vishwadeep108@gmail.com>
2025-03-31 12:12:51 +0530

    json file added

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const express = require("express");
 const app = express(); //Creates an Express application. The express() function is a top-level function exported by the express module
 
 const port = 4000;
+const github = {
+  message: "Not Found",
+  documentation_url: "https://docs.github.com/rest",
+  status: "404",
+};
 
 app.get("/", (req, res) => {
   res.send("hello buddy !!");
@@ -19,5 +24,9 @@ app.get("/youtube", (req, res) => {
 
 app.listen(process.env.PORT, () => {
   console.log(`exaple app running on port ${port}`);
+});
+
+app.get("/github", (req, res) => {
+  res.json(github);
 });
 //The .listen() method in Express.js starts the server and makes it listen for incoming requests on a specified port.
